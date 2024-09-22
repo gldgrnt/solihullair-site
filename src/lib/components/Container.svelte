453bfb3b32e555ebdd2b5bbe190d89cf43a98d1f
Gilad Grant <ggrant966@gmail.com>
2024-09-22 18:02:41 +0100

    wip: add footer elements

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,8 @@
+<section class="container"><slot /></section>
+
+<style lang="scss">
+	section {
+		--background-color: var(--sa-colour-snow);
+		padding: 60px 80px 30px;
+	}
+</style>
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,15 @@
-<footer>Footer</footer>
+<script>
+	import Container from './Container.svelte';
+</script>
+
+<footer>
+	<Container>
+		<a href="/"><img class="footer__logo" src="/logo-bw.svg" alt="Solihull Air Logo" /></a>
+	</Container>
+</footer>
+
+<style lang="scss">
+	.footer__logo {
+		max-height: 75px;
+	}
+</style>
