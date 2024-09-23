052ecf410951710f521f19f8114d59e93c05469e
Gilad Grant <ggrant966@gmail.com>
2024-09-23 14:56:39 +0100

    wip: mobile menu

--- src/lib/components/Header/BurgerMenu.svelte
+++ src/lib/components/Header/BurgerMenu.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
 	export let active = false;
+
+	function toggleActive() {
+		return (active = !active);
+	}
 </script>
 
-<button type="button" aria-label="Open menu" class="burger-menu" class:active
-	><div class="burger-menu__icon"></div>
+<button
+	type="button"
+	aria-label="Open menu"
+	class="burger-menu"
+	class:active
+	on:click={toggleActive}
+>
+	{#each { length: 3 } as _}
+		<span class="burger-menu__row"></span>
+	{/each}
 </button>
 
 <style lang="scss">
 	.burger-menu {
 		display: inline-flex;
 		padding: var(--sa-spacing-sm);
+		flex-direction: column;
+		gap: 6px;
+		border: none;
+		border-radius: var(--sa-spacing-xxs);
+		background-color: var(--sa-colour-platinum);
+
+		&:hover,
+		&:focus {
+			filter: brightness(90%);
+		}
+
+		&.active {
+			background-color: var(--sa-colour-carbon);
+			filter: brightness(150%);
+
+			.burger-menu__row {
+				background-color: var(--sa-colour-platinum);
+			}
+		}
 	}
 
-	.burger-menu__icons {
-		background: red;
+	.burger-menu__row {
+		background-color: var(--sa-colour-carbon);
+		opacity: 0.5;
 		height: 4px;
+		width: 24px;
+		border-radius: 1px;
 	}
 </style>
--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -42,6 +42,8 @@
 
 <style>
 	.header {
+		position: sticky;
+		top: 0;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
