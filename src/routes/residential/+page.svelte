228d67345885fa52115466352965230e71437c28
Gilad Grant <ggrant966@gmail.com>
2024-09-29 18:42:43 +0100

    remove unused code

--- src/routes/residential/+page.svelte
+++ src/routes/residential/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Section from '$lib/components/Section.svelte';
-	import Container from '$lib/components/Container.svelte';
+	// import Section from '$lib/components/Section.svelte';
+	// import Container from '$lib/components/Container.svelte';
 	import GetInTouch from '$lib/components/PageSections/GetInTouch.svelte';
 	import PageTitle from '$lib/components/PageSections/PageTitle.svelte';
 	import Services from '$lib/components/PageSections/Services';
@@ -20,12 +20,6 @@
 	description="Air conditioning offers an incredibly efficient and cost-effective way to both cool and heat
 		your home."
 />
-
-<Section>
-	<Container>
-		<!-- <ImageCarousel /> -->
-	</Container>
-</Section>
 
 <Services title="For residential properties, SolihullAir can offer the following services:">
 	{#each services as { title, description }}
