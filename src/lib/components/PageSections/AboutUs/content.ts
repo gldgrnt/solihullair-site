43a58351c80d3af872530d9d9f3831a50c6145b2
Gilad Grant <ggrant966@gmail.com>
2024-09-30 18:00:22 +0100

    remove extra content

--- src/lib/components/PageSections/AboutUs/content.ts
+++ src/lib/components/PageSections/AboutUs/content.ts
@@ -3,7 +3,7 @@ import { routes } from '$lib/data/routes';
 export const text = {
 	emphasised: 'We’re based in Solihull and cover the Midlands, Worcestershire and Shropshire',
 	normal:
-		'With over 30 years’ experience, our skilled technicians work with various brands to create expert solutions tailored to your needs. We cover all aspects of air conditioning and refrigeration services, including installation, maintenance, and repair services.'
+		"With over 30 years' experience, our skilled technicians work with various brands to create expert solutions tailored to your needs."
 };
 
 export const link = {
